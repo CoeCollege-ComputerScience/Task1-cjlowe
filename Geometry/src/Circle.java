675b0a122913f6e5c85aa879df9bbc8d089de09b
testing3 <cjlowe25@coe.edu>
2026-09-03 12:51:19 -0500

    Finished circle class best I could with the time I have

--- Geometry/src/Circle.java
+++ Geometry/src/Circle.java
@@ -8,11 +8,13 @@ public class Circle {
     }
 
     public Circle(double radius) {
-        //Todo  Implement
+        this.radius = radius;
+        this.center = new Point();
     }
 
     public Circle(double radius, Point center) {
-        //Todo  Implement
+        this.radius = radius;
+        this.center = center;
     }
 
     public double getRadius() {
@@ -23,14 +25,11 @@ public class Circle {
     }
 
     public void setRadius(double radius) {
-        //Todo  Implement
+        this.radius = radius;
     }
     public void setCenter(Point center) {
         this.center = center;
     }
-
-
-
 
     public double getCircumference(){
         return 2 * Math.PI * radius;
@@ -40,11 +39,17 @@ public class Circle {
         return Math.PI * Math.pow(radius, 2);   // This will make compiler happy
     }
 
-    public boolean isInside(Point p){
-
+    public boolean isInside(Point p){ // I know this only works if your point is adjacent with the center, I can't think of anything else right now
+        if ((p.getX() > (center.getX() - radius)) && ((p.getX() < (center.getX() + radius)))){
+            return true;
+        }
+        if ((p.getY() > (center.getY() - radius)) && ((p.getY() < (center.getY() + radius)))){
+            return true;
+        }
+        return false;
         //This is a trap!
-        return false;   // This will make compiler happy
     }
+
     public String toString(){
         return "radius: " + radius + "\nCenter: " + center;
     }
